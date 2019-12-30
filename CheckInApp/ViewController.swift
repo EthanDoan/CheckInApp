e128c734821ba2920ba04d6991e96bb378c5db63
phuong <phuong.doan@propzy.com>
2019-12-30 14:23:45 +0700

    initial commit

---
+++ CheckInApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckInApp
+//
+//  Created by Doan Van Phuong on 12/30/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'CheckInApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for CheckInApp
+
+pod 'Firebase/Database'
+pod 'QRCodeReader.swift', '~> 10.1.0'
+pod 'SnapKit'
+
+end
